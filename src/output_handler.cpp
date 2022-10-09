76f41aec6bd8e6be2df2de129c11c8a06b7baa12
PMylon <pmylon20@gmail.com>
2022-10-09 13:48:22 +0300

    Disable acm cdc uart for frdm_k64f

--- src/output_handler.cpp
+++ src/output_handler.cpp
@@ -20,28 +20,28 @@
 #include <zephyr/usb/usb_device.h>
 #include <zephyr/drivers/uart.h>
 
-BUILD_ASSERT(DT_NODE_HAS_COMPAT(DT_CHOSEN(zephyr_console), zephyr_cdc_acm_uart),
-	     "Console device is not ACM CDC UART device");
+// BUILD_ASSERT(DT_NODE_HAS_COMPAT(DT_CHOSEN(zephyr_console), zephyr_cdc_acm_uart),
+// 	     "Console device is not ACM CDC UART device");
 
-void SetupOutput()
-{
-	const struct device *dev_console = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
-	uint32_t dtr = 0;
+// void SetupOutput()
+// {
+// 	const struct device *dev_console = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
+// 	uint32_t dtr = 0;
 
-	if (usb_enable(NULL)) {
-		printk("Serial output not initialized!\n");
-		return;
-	}
+// 	if (usb_enable(NULL)) {
+// 		printk("Serial output not initialized!\n");
+// 		return;
+// 	}
 
-	/* Poll if the DTR flag was set */
-	while (!dtr) {
-		uart_line_ctrl_get(dev_console, UART_LINE_CTRL_DTR, &dtr);
-		/* Give CPU resources to low priority threads. */
-		k_sleep(K_MSEC(100));
-	}
+// 	/* Poll if the DTR flag was set */
+// 	while (!dtr) {
+// 		uart_line_ctrl_get(dev_console, UART_LINE_CTRL_DTR, &dtr);
+// 		/* Give CPU resources to low priority threads. */
+// 		k_sleep(K_MSEC(100));
+// 	}
 
-	printk("Serial output initialized!\n");
-}
+// 	printk("Serial output initialized!\n");
+// }
 
 void HandleOutput(tflite::ErrorReporter *error_reporter, int kind)
 {
